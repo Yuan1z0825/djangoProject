4f3d92e8a694d4c4dd01248dc03f1eb73902172a
yuanyz0825 <yuanyz2000@gmail.com>
2023-08-22 14:22:18 +0800

    Initial commit

---
+++ app01/views.py
@@ -0,0 +1,122 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+# 关于介绍我们的自己html路径
+def aboutUs(request):
+    return render(request, 'aboutUs/index.html')
+
+def us_history(request):
+    return render(request, 'aboutUs/history.html')
+
+def us_books(request):
+    return render(request, 'aboutUs/books.html')
+
+def us_software(request):
+    return render(request, 'aboutUs/software.html')
+
+def us_train(request):
+    return render(request, 'aboutUs/train.html')
+
+def us_news(request):
+    return render(request, 'aboutUs/news.html')
+
+def us_foreword(request):
+    return render(request, 'aboutUs/foreword.html')
+
+def us_join(request):
+    return render(request, 'aboutUs/join.html')
+
+
+
+# 关于研究方向的html路径
+
+def researchDir(request):
+    return render(request, 'researchDir/index.html')
+
+def researchdeprivation(request):
+    return render(request, 'researchDir/deprivation.html')
+
+def researchmonitor(request):
+    return render(request, 'researchDir/monitor.html')
+
+def researchregulation(request):
+    return render(request, 'researchDir/regulation.html')
+
+def researcher(request):
+    return render(request, 'researchers/index.html')
+
+def researcher_teacher(request):
+    return render(request, 'researchers/teachers.html')
+
+def researcher_graduate(request):
+    return render(request, 'researchers/graduate.html')
+
+def researcher_visiting(request):
+    return render(request, 'researchers/visiting.html')
+
+def researcher_finish(request):
+    return render(request, 'researchers/finish.html')
+
+
+
+
+
+# 关于培养方案的html路径
+def cultivation(request):
+    return render(request, 'cultivation/index.html')
+
+def cultivation_graduate(request):
+    return render(request, 'cultivation/graduate.html')
+
+
+
+
+# 关于成果的html路径
+def mainAch(request):
+    return render(request, 'mainAch/index.html')
+
+def mainAch_fund(request):
+    return render(request, 'mainAch/fund.html')
+
+def mainAch_paper(request):
+    return render(request, 'mainAch/paper.html')
+
+def mainAch_patent(request):
+    return render(request, 'mainAch/patent.html')
+
+def mainAch_prize(request):
+    return render(request, 'mainAch/prize.html')
+
+
+
+
+
+# 关于合作交流的html路径
+def cooStudy(request):
+    return render(request, 'cooStudy/index.html')
+
+def cooStudy_visiting(request):
+    return render(request, 'cooStudy/visiting.html')
+
+
+
+
+# 睡眠工程的html路径
+def sleepEng(request):
+    return render(request, 'sleepEng/index.html')
+
+def sleepEng_hardware(request):
+    return render(request, 'sleepEng/hardware.html')
+
+def sleepEng_service(request):
+    return render(request, 'sleepEng/service.html')
+
+def sleepEng_cooperation(request):
+    return render(request, 'sleepEng/cooperation.html')
+
+
+
---
+++ djangoProject/urls.py
@@ -0,0 +1,72 @@
+"""djangoProject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.views.generic import RedirectView
+
+from app01 import views
+
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('index.html', RedirectView.as_view(url='/', permanent=False)),
+
+    # 关于介绍我们的自己html路径
+    path('aboutUs/index.html', views.aboutUs, name='aboutUs'),
+    path('aboutUs/history.html', views.us_history, name='us_history'),
+    path('aboutUs/books.html', views.us_books, name='us_books'),
+    path('aboutUs/software.html', views.us_software, name='us_software'),
+    path('aboutUs/train.html', views.us_train, name='us_train'),
+    path('aboutUs/news.html', views.us_news, name='us_news'),
+    path('aboutUs/foreword.html', views.us_foreword, name='us_foreword'),
+    path('aboutUs/join.html', views.us_join, name='us_join'),
+
+
+    #关于研究方向的html路径
+    path('researchDir/index.html', views.researchDir, name='researchDir'),
+    path('researchDir/deprivation.html', views.researchdeprivation, name='researchdeprivation'),
+    path('researchDir/monitor.html', views.researchmonitor, name='researchmonitor'),
+    path('researchDir/regulation.html', views.researchregulation, name='researchregulation'),
+
+
+    #关于研究人员的html路径
+    path('researchers/index.html', views.researcher, name='researcher'),
+    path('researchers/teachers.html', views.researcher_teacher, name='reasercher_teacher'),
+    path('researchers/graduate.html', views.researcher_graduate, name='reasercher_graduate'),
+    path('researchers/visiting.html', views.researcher_visiting, name='reasercher_visiting'),
+    path('researchers/finish.html', views.researcher_finish, name='reasercher_finish'),
+
+    #关于培养方案的html路径
+    path('cultivation/index.html', views.cultivation, name='cultivation'),
+    path('cultivation/graduate.html', views.cultivation_graduate, name='cultivation_graduate'),
+
+    #关于成果的html路径
+    path('mainAch/index.html', views.mainAch, name='mainAch'),
+    path('mainAch/fund.html', views.mainAch_fund, name='mainAch_fund'),
+    path('mainAch/paper.html', views.mainAch_paper, name='mainAch_paper'),
+    path('mainAch/patent.html', views.mainAch_patent, name='mainAch_patent'),
+    path('mainAch/prize.html', views.mainAch_prize, name='mainAch_prize'),
+
+    #关于合作交流的html路径
+    path('cooStudy/index.html', views.cooStudy, name='cooStudy'),
+    path('cooStudy/visiting.html', views.cooStudy_visiting, name='cooStudy_visiting'),
+
+    #睡眠工程的html路径
+    path('sleepEng/index.html', views.sleepEng, name='sleepEng'),
+    path('sleepEng/hardware.html', views.sleepEng_hardware, name='sleepEng_hardware'),
+    path('sleepEng/service.html', views.sleepEng_service, name='sleepEng_service'),
+    path('sleepEng/cooperation.html', views.sleepEng_cooperation, name='sleepEng_cooperation'),
+]
